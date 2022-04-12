e425654e0664f1ef6ca7cd425167bcaabafd406e
anthoniraj <anthoniraja@gmail.com>
2022-04-13 04:21:46 +0530

    Annonymous Inner Class Added

--- src/com/anigna/java/collection/AnonymousClassDemo.java
+++ src/com/anigna/java/collection/AnonymousClassDemo.java
@@ -1,5 +1,32 @@
 package com.anigna.java.collection;
 
-public class AnonymousClassDemo {
-    
+@FunctionalInterface
+interface Greeting{
+    public void display();
 }
+
+@FunctionalInterface
+interface Calculator{
+    public int add(int a, int b);
+}
+
+public class AnonymousClassDemo{
+
+    public static void main(String[] args) {
+       Greeting greet = new Greeting() {
+        @Override
+        public void display() {
+            System.out.println("Welcome to Lambda!");
+            
+        }          
+       };
+       greet.display();
+
+       Greeting greetNew = ()->System.out.println("Welcome to Lambda!");
+       greetNew.display();
+
+       Calculator calc = (a, b) -> (a+b);
+       System.out.println("The addition is "+calc.add(10, 20));
+    }
+
+ }
